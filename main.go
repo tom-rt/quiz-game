4dc55b5024bdddc1c1468d402a6972b220c8e477
Thomas Raout <thomraout@gmail.com>
2021-01-11 18:45:53 +0100

    channel created, game played

--- main.go
+++ main.go
@@ -5,11 +5,50 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"bufio"
+    "log"
 )
+
+func playGame(ch chan bool) {
+	file, err := os.Open("./problems.csv")
+	if err != nil {
+        log.Fatal("Impossible to open file:", err)
+    }
+    defer file.Close()
+    scanner := bufio.NewScanner(file)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	var line string = ""
+	var answer string = ""
+	var question string = ""
+	var solution string = ""
+
+	for scanner.Scan() {
+		line = scanner.Text()
+		splittedQuestion := strings.Split(line, ",")
+		question = splittedQuestion[0]
+		solution = splittedQuestion[1]
+		fmt.Println(question, "?")
+		answer, _ = reader.ReadString('\n')
+		answer = strings.TrimSuffix(answer, "\n")
+		if (answer == solution) {
+			fmt.Println("Good answer !")
+		} else {
+			fmt.Println("Wrong answer !")
+		}
+	}
+	ch <- true
+}
 
 func main() {
 	limit := parseArgs()
-	fmt.Println(limit)
+	if (limit == -1) {
+		return
+	}
+	ch := make(chan bool)
+	go playGame(ch)
+	<-ch
 }
 
 func parseArgs() int {
